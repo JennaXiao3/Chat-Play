4ba7e5b0a4997271da230e7fd776bec680fdeab4
Productivity <grantchen2021@gmail.com>
2024-05-18 10:30:50 -0400

    added gitignore, and some server starter code

---
+++ server/src/handlers.py
@@ -0,0 +1,15 @@
+def create_room(host_name: str):
+    return {
+        "room_id": "2131",
+        "room_code": "123",
+        "user_id": "fasdfhsa23"
+    }
+    
+
+def join_room(room_code: str):
+    return {
+        "user_id": "213123"
+    }
+    
+def start_room(user_id: str, room_code: str):
+    return {}
--- server/src/server.py
+++ server/src/server.py
@@ -1,16 +1,29 @@
-from flask import Flask, render_template
-from flask_cors import CORS
+from flask import Flask
 from flask_socketio import SocketIO, emit
-import time
+import json
+
+import handlers
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 @socketio.on('message')
-def handle_message(message):
-    print('Received message:', message)
-    emit('response', {'data': f'server says hi {client_count}'})
+def handle_message(message: str):
+    data = json.loads(message)
+    
+    match data["action"]:
+        case "create_room":
+            handlers.create_room(data["user_id"])
+            
+        case "join_room":
+            handlers.join_room(data["room_id"])
+            
+        case "start_room":
+            handlers.start_room(data["user_id"], data["room_code"])
+        
+        case _:
+            raise ValueError("no action matched")
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000, debug=True)
