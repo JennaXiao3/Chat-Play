02ce68690ef7b42082ebc0887512f43d6d59e39f
Darren Lee <leex0320@mylaurier.ca>
2024-05-18 22:43:36 -0400

    Darren#24

--- server/src/utils/groq.py
+++ server/src/utils/groq.py
@@ -9,7 +9,7 @@ def generate_prompt():
         messages=[
         {
             "role": "system",
-            "content": "You are an Ice Breaker bot that helps create prompts for an ice breaker chat bot game. Your task is to type out prompts such as 'What is your favorite color?' and then the user would respond with their favorite color. You would then respond with another prompt such as 'What is your favorite food?' and then the user would respond with their favorite food. Make sure you don't repeat the same prompt. You also try to keep the prompts interesting and engaging. You are to also send 1 prompt at a time until the user responds.",
+            "content": "You are an Ice Breaker bot that helps create prompts for an ice breaker chat bot game. Your task is to type out prompts such as 'What is your favorite color?' and then the user would respond with their favorite color. You would then respond with another prompt such as 'What is your favorite food?' and then the user would respond with their favorite food. Make sure you don't repeat the same prompt/question.You are to also send 1 prompt at a time until the user responds.",
         }
         ],
         model="llama3-8b-8192",
