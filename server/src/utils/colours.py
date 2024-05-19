add5ee3686474faeb4f4da1147383921a4e99729
Brandon Lai-Cheong <blaicheo@uwaterloo.ca>
2024-05-18 21:21:23 -0400

    more colours

--- server/src/utils/colours.py
+++ server/src/utils/colours.py
@@ -1,6 +1,8 @@
 import random
 
-colours = [ "F5F91B", "1BF9C4", "FF555", "86FF73" ]
+colours = [ "F5F91B", "1BF9C4", "FF555", "86FF73", "F18F01", "048BA8", "99C24D", "EC058E" ]
 
 def generate_colour() -> str:
-    return random.choice(colours) 
+    random.shuffle(colours)
+    return colours.pop(0)
+
