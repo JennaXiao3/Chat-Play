743189c50b367c8d746a91a09552c416d1f02e2a
Brandon Lai-Cheong <blaicheo@uwaterloo.ca>
2024-05-18 22:02:00 -0400

    colour work

--- server/src/utils/colours.py
+++ server/src/utils/colours.py
@@ -1,10 +1,11 @@
 import random
+import copy
 
 colours = [ "F5F91B", "1BF9C4"]
 
 # "FFF555", "86FF73", "F18F01", "048BA8", "99C24D", "EC058E" ]
 
-curr_colours = colours.copy()
+curr_colours = copy.deepcopy(colours)
 
 def generate_colour() -> str:
     random.shuffle(curr_colours)
